6cb052cc78a708555c344d138ad70e56056fab0e
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-31 22:56:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   if array.length == 1 
-    return #{array}
+    return "#{array}"
   elsif array.length == 2 
     return array.join(" and ")
+  elsif array.length >=3
+    array[-1] = " and #{array}"
+  
 end
 end
