a533194467d80222f40dec4137506908ea9afdf7
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-31 23:08:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array[0]
   elsif array.length == 2 
     return array.join(" and ")
-  else
-    return array[0..2].join(", ") = " and " + array[-1]
+  elsif array.length == 3
+    return array[0..-1].join(", ") = " and " + array[-1]
 end
 end
